0c015813d7bc0913c7926c20ea43d416c8b1083b
Austin McDaniel <amcdaniel2@gmail.com>
2016-07-07 10:49:57 -0400

    (security): adding security hook

--- petstore/src/config/Express.ts
+++ petstore/src/config/Express.ts
@@ -5,11 +5,12 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as yaml from 'js-yaml';
 import * as cors from 'cors';
-import * as jwt from 'jsonwebtoken';
 import * as config from 'config';
+import { logger } from '../common/logging';
 
 import { useExpressServer } from 'routing-controllers';
 import * as swaggerTools from 'swagger-tools';
+import * as expressJwt from 'express-jwt';
 
 export class ExpressConfig {
 
@@ -27,11 +28,18 @@ export class ExpressConfig {
   }
 
   setupSwagger() {
+    // resolve the spec
     const spath = path.resolve('swagger.yml');
     const file = fs.readFileSync(spath, 'utf8');
     const spec = yaml.safeLoad(file);
-    // const jwtSecret = config.get('auth.jwt_secret');
+    
+    // init jwt middleware
+    const jwtSecret = config.get('auth.jwt_secret').toString();
+    let jwt = expressJwt({
+      secret: new Buffer(jwtSecret, 'base64')
+    });
 
+    // setup middleware swagger middleware in express
     swaggerTools.initializeMiddleware(spec, (middleware) => {
       this.app.use(middleware.swaggerMetadata());
       
@@ -39,20 +47,25 @@ export class ExpressConfig {
         validateResponse: true
       }));
 
-      /*
       this.app.use(middleware.swaggerSecurity({
-        jwt: function(req, authOrSecDef, scopes, cb) {
-          jwt(jwtSecret, req, cb);
+        jwt_token: (req, authOrSecDef, scopes, cb) => {
+          jwt(req, req.res, (err) => {
+            if (req.userName === undefined) {
+              return cb(new Error('Access Denied!'));
+            } else {
+              logger.info(`${req.userName} authorized`, req.userName);
+              return cb(null);
+            }
+          });
         }
       }));
-      */
 
       this.app.use(middleware.swaggerUi());
     });
   }
 
   setupControllers() {
-    const controllersPath = path.resolve('controllers/');
+    const controllersPath = path.resolve('dist', 'controllers');
     useExpressServer(this.app, {
       controllerDirs: [ controllersPath ]
     });
