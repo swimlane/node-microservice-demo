b3a1d8997a3445724230a7af45e903874e1ca954
Austin McDaniel <amcdaniel2@gmail.com>
2016-07-07 10:04:46 -0400

    (sec): adding security and better controller config

--- petstore/src/config/Express.ts
+++ petstore/src/config/Express.ts
@@ -5,6 +5,8 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as yaml from 'js-yaml';
 import * as cors from 'cors';
+import * as jwt from 'jsonwebtoken';
+import * as config from 'config';
 
 import { useExpressServer } from 'routing-controllers';
 import * as swaggerTools from 'swagger-tools';
@@ -28,16 +30,29 @@ export class ExpressConfig {
     const spath = path.resolve('swagger.yml');
     const file = fs.readFileSync(spath, 'utf8');
     const spec = yaml.safeLoad(file);
+    // const jwtSecret = config.get('auth.jwt_secret');
 
     swaggerTools.initializeMiddleware(spec, (middleware) => {
       this.app.use(middleware.swaggerMetadata());
-      this.app.use(middleware.swaggerValidator());
+      
+      this.app.use(middleware.swaggerValidator({
+        validateResponse: true
+      }));
+
+      /*
+      this.app.use(middleware.swaggerSecurity({
+        jwt: function(req, authOrSecDef, scopes, cb) {
+          jwt(jwtSecret, req, cb);
+        }
+      }));
+      */
+
       this.app.use(middleware.swaggerUi());
     });
   }
 
   setupControllers() {
-    const controllersPath = path.resolve(__dirname, '../controllers');
+    const controllersPath = path.resolve('controllers/');
     useExpressServer(this.app, {
       controllerDirs: [ controllersPath ]
     });
