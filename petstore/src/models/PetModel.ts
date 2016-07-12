9998df43d533d1a30054c7de9d032fb1892b109f
Austin McDaniel <amcdaniel2@gmail.com>
2016-07-12 09:11:26 -0400

    experiment, does not work

--- petstore/src/models/PetModel.ts
+++ petstore/src/models/PetModel.ts
@@ -1,6 +1,16 @@
+import * as Swagger from 'swagger-client';
+import { Service } from 'typedi';
+
+@Service()
 export class PetModel {
 
-  constructor(public name: string) {
+  client: any;
+
+  constructor() {
+    this.client = new Swagger({
+      url: 'http://petstore.swagger.io/v2/swagger.json',
+      usePromise: true
+    });
   }
 
 }
