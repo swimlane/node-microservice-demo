364081535acd82697ffaa32a8254445780681a54
Austin McDaniel <amcdaniel2@gmail.com>
2016-07-13 18:57:27 -0400

    build fix and reducing noise w/ ts-helpers

--- petstore/src/index.ts
+++ petstore/src/index.ts
@@ -1,8 +1,9 @@
 'use strict';
 
+import 'ts-helpers';
 import 'source-map-support/register';
 import 'reflect-metadata';
 import './types';
-import { Application } from './config/Application';
 
+import { Application } from './config/Application';
 export default new Application();
