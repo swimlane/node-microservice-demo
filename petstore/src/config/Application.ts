b48b54da3ca1a2e00d761df32d3934e53cc91aa2
Austin McDaniel <amcdaniel2@gmail.com>
2016-07-13 18:32:58 -0400

    better port handling

--- petstore/src/config/Application.ts
+++ petstore/src/config/Application.ts
@@ -10,15 +10,20 @@ export class Application {
   constructor()  {
     this.express = new ExpressConfig();
 
-    const port = config.get('express.port');
+    const port = config.get('ports.http');
+    const debugPort = config.get('ports.debug');
+
     this.server = this.express.app.listen(port, () => {
       logger.info(`
         ------------
         Server Started!
-        Express: http://localhost:${port}
-        Debugger: http://127.0.0.1:8080/?ws=127.0.0.1:8080&port=5858
-        Swagger Docs: http://localhost:${port}/docs
-        Swagger Spec: http://localhost:${port}/api-docs
+
+        Http: http://localhost:${port}
+        Debugger: http://127.0.0.1:${port}/?ws=127.0.0.1:${port}&port=${debugPort}
+        Health: http://localhost:${port}/ping
+
+        API Docs: http://localhost:${port}/docs
+        API Spec: http://localhost:${port}/api-docs
         ------------
       `);
     });
