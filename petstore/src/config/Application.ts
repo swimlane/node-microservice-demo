abca43926c7fef7cc1e668b1121ace16fb0cc7db
Austin McDaniel <amcdaniel2@gmail.com>
2016-07-13 10:27:47 -0400

    better debugging

--- petstore/src/config/Application.ts
+++ petstore/src/config/Application.ts
@@ -9,13 +9,14 @@ export class Application {
 
   constructor()  {
     this.express = new ExpressConfig();
-    
+
     const port = config.get('express.port');
     this.server = this.express.app.listen(port, () => {
       logger.info(`
         ------------
         Server Started!
         Express: http://localhost:${port}
+        Debugger: http://127.0.0.1:8080/?ws=127.0.0.1:8080&port=5858
         Swagger Docs: http://localhost:${port}/docs
         Swagger Spec: http://localhost:${port}/api-docs
         ------------
